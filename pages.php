1861d505e98dbca7eb6fe45dd18e99dd0b6ecc22
Gokay Biz <gkymhmt@gmail.com>
2017-05-21 20:31:11 +0300

    Optimization

--- pages.php
+++ pages.php
@@ -1,19 +1,21 @@
 <?php
 $p = @$_GET["p4ge"];
-if (!empty($p)) {
-	switch ($p) {
-		case 'test':
-			$g = "deneme";
-			break;
-		
-		default:
-			$g = 404;
-			break;
-	}
-	if ($g == 404) die(include("partials/pages/$g.php"));
-	$g = "partials/pages/$g.php";
-	if (!is_file($g)) exit("Sayfa tanimlanmis ama dosya yok");
-}else
-	$g = "partials/pages/home.php";
+
+switch ($p) {
+	case 'test':
+		$g = "deneme";
+		break;
+	
+	default:
+		$g = 404;
+		if (empty($p))
+			$g = "home";
+		break;
+}
+if ($g == 404) { die(include("partials/pages/err/$g.php")); }
+
+$g = "partials/pages/$g.php";
+if (!is_file($g)) exit("Sayfa tanimlanmis ama dosya yok");
+
 $g = (new Obfucator($g))->encode();
 ?>
