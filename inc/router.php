df8a2c221faa5b82a2de62ef06d8f42e9b4cbf5f
gokaybiz <gkyhmt@gmail.com>
2025-02-19 17:45:56 +0100

    feat: better routing

---
+++ inc/router.php
@@ -0,0 +1,68 @@
+<?php
+class Router
+{
+    protected $routes = [];
+
+    /**
+     * Registers a route and its handling function.
+     *
+     * @param string $route The route to register.
+     * @param callable $callback The callback function to handle the route.
+     * @return void
+     */
+    public function add($route, callable $callback): void
+    {
+        // Normalize the route.
+        $route = trim($route, "/");
+        $this->routes[$route] = $callback;
+    }
+
+    /**
+     * Dispatches the current URI request.
+     *
+     * @param string $uri The URI to dispatch.
+     * @return void
+     */
+    public function dispatch($uri): void
+    {
+        $uri = trim($uri, "/");
+        if (isset($this->routes[$uri])) {
+            call_user_func($this->routes[$uri]);
+        } else {
+            $this->handle404();
+        }
+    }
+
+    /**
+     * Handles the 404 error page.
+     *
+     * @return void
+     */
+    public function handle404(): void
+    {
+        if (isset($this->routes["404"])) {
+            call_user_func($this->routes["404"]);
+        } else {
+            header("HTTP/1.0 404 Not Found");
+            echo "404 Not Found";
+        }
+    }
+
+    /**
+     * Loads a page from disk.
+     *
+     * @param string $page The page to load.
+     * @return string|bool
+     */
+    public function loadPageFromDisk(string $page): string|bool
+    {
+        $path = __DIR__ . "/../partials/pages/$page.php";
+        if (file_exists($path)) {
+            return $path;
+        } else {
+            $this->handle404();
+            return false;
+        }
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <?php
-define('PRIV', '1');
+define("PRIV", "1");
 
-require_once('inc/config.php');
-include "pages.php";
+require_once "inc/config.php";
+require_once "inc/router.php";
+require_once "inc/obfuscator.php";
+
 include "partials/header.php";
-?>
-        <?php echo $g; ?>
-<?php
+
+$router = new Router();
+$router->add("/", function () use ($router) {
+    $page = $router->loadPageFromDisk("home");
+
+    if ($page !== false) {
+        $obfuscator = new Obfucator($page);
+        echo $obfuscator->encode();
+    }
+    // Don't worry if it doesn't exist, it will be handled by the router.
+});
+
+$route = isset($_GET["page"]) ? $_GET["page"] : "/";
+$router->dispatch($route);
+
 include "partials/footer.php";
+
 ?>
